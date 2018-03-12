72f8422441bc4a679f62f105e7ad8499654baeb2
gabzon <gab.zambrano@gmail.com>
2018-03-12 16:37:45 +0100

    homepage top padding

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -7,7 +7,7 @@ Template Name: Home page
 
 @section('content')
   @while(have_posts()) @php(the_post())
-    <div class="home-content" style="margin-top:-40px; padding-top:30px;">
+    <div class="home-content" style="margin-top:-40px;">
       @include('partials.content-page')
     </div>
 
