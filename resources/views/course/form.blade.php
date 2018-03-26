8091fb6918120affb786ddf9a453465db0af6f10
gabzon <gab.zambrano@gmail.com>
2018-03-26 17:06:28 +0200

    test form 6

--- resources/views/course/form.blade.php
+++ resources/views/course/form.blade.php
@@ -2,7 +2,7 @@
 <section id="inscription">
   <div class="ui form">
     @php( $form_id = get_post_meta($post->ID,'course_form',true) )
-    @if ($form_id === '23')
+    @if ($form_id)
       {{-- @php( $page = get_page_by_title( $form_title ) )
       @php( $content = apply_filters('the_content', $page->post_content) )
       @php
@@ -12,11 +12,11 @@
     <br>
     <hr> --}}
     @php
-    gravity_form(23, false, false, false, '', true, 12);
+    gravity_form($form_id, false, false, false, '', true, 12);
     @endphp
   @else
     @php
-    gravity_form($form_id, false, false, false, '', true, 12);
+    gravity_form(23, false, false, false, '', true, 12);
     @endphp
   @endif
 </div>
