46c357c6d0ca00e192bf88719bddfb8a111c93ac
gabzon <gab.zambrano@gmail.com>
2018-08-28 11:43:35 +0200

    iphone teachers pictures debugged

--- app/teachers.php
+++ app/teachers.php
@@ -24,22 +24,23 @@ function display_person($indice, $id, $email, $photo, $first_name, $last_name, $
   </div>
 
 
-
   <div class="card grow pointer mb4">
-    <?php if ($photo): ?>
-      <a class="" type="button" data-toggle="modal" data-target="#myModal-<?= $indice; ?>">
-        <img class="card-img-top img-fluid " src="<?= wp_get_attachment_url($photo); ?>" alt="Card image cap" />
-      </a>
-    <?php else: ?>
-      <img class="card-img-top card-img img-fluid " src="<?php echo get_avatar_url( $email, array('size'  => 512) ); ?>" alt="Card image cap"/>
-    <?php endif; ?>
+    <a class="card-link" data-toggle="modal" data-target="#myModal-<?= $indice; ?>">
+      <?php if ($photo): ?>
+        <img class="card-img-top img-fluid" src="<?= wp_get_attachment_url($photo); ?>" alt="Card image cap" />
+      <?php else: ?>
+        <img class="card-img-top card-img img-fluid" src="<?php echo get_avatar_url( $email, array('size'  => 512) ); ?>" alt="Card image cap"/>
+      <?php endif; ?>
+    </a>
     <div class="card-body text-center">
       <?php $dancefloor_options = get_option('dancefloor_settings'); ?>
-      <?php if ( $dancefloor_options['df_display_team_member_name'] == 'nickname'): ?>
-        <h4 class="card-title ttc"><?= esc_html( get_the_author_meta('display_name',$id) ); ?></h4>
-      <?php else: ?>
-        <h4 class="card-title"><?= esc_html( $first_name ); ?> <?= esc_html( $last_name ); ?></h4>
-      <?php endif; ?>
+
+        <?php if ( $dancefloor_options['df_display_team_member_name'] == 'nickname'): ?>
+          <h4 class="card-title ttc"><?= esc_html( get_the_author_meta('display_name',$id) ); ?></h4>
+        <?php else: ?>
+          <h4 class="card-title"><?= esc_html( $first_name ); ?> <?= esc_html( $last_name ); ?></h4>
+        <?php endif; ?>
+
       <p class="card-text text-muted"><?= esc_html($title); ?></p>
       <?php if (get_user_meta($id,'skills')): ?>
         <h5><?php _e('Skills','sage'); ?></h5>
@@ -54,6 +55,8 @@ function display_person($indice, $id, $email, $photo, $first_name, $last_name, $
         </span>
       <?php endif; ?>
     </div>
+
+
     <div class="card-footer text-center">
       <a class="btn-social btn-google-plus" href="mailto:<?= esc_html($email); ?>"><i class="fa fa-envelope"></i></a>
       <?php if (get_user_meta($id,'facebook',true)): ?>
@@ -63,6 +66,7 @@ function display_person($indice, $id, $email, $photo, $first_name, $last_name, $
         <a class="btn-social btn-google-plus" href="tel:<?= get_user_meta($id,'phone',true); ?>"><i class="fa fa-phone"></i></a>
       <?php endif; ?>
     </div>
+
   </div>
 
   <?php
